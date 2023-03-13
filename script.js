7cbf04cf4127b4d4c2dbb7615e7d5bf82398b0ed
Rayyan <rayyanrsarkhot@gmail.com>
2023-03-13 16:08:14 -0400

    Added randomization for return output

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 class Colors {
 
-    constructor() {}
+    constructor() {
+    
+        // Arrays hold the three seperate color output options.
+        this._first = ["red", "blue", "green"];
+        this._second = ["black", "white", "orange"];
+        this._third = ["pink", "purple", "gray"];
+    
+    }
+    
+    // Method chooses colors randomly.
+    chooseColors() {
 
-    // Arrays hold the three seperate color output options.
-    first = ["red", "blue", "green"];
-    second = ["black", "white", "orange"];
-    third = ["pink", "purple", "gray"];
+        // Generates random colors and puts in output array.
+        let output = [this._first[Math.floor(Math.random() * (this._first.length))], 
+            this._second[Math.floor(Math.random() * (this._second.length))],
+            this._third[Math.floor(Math.random() * (this._third.length))]];
+
+        return output;
+
+    } 
 
 }
+
